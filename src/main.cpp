b08b2f9faa0329cdec554562583f224e5d17c1b3
Luke <ruzovkaa@gmail.com>
2020-09-18 13:10:47 +0200

    CCS811 mesurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
-#include <Arduino.h>
+#include "Adafruit_CCS811.h"
+
+Adafruit_CCS811 ccs;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Serial.println("CCS811 test");
+
+  if(!ccs.begin()){+
+    Serial.println("Chyba senzoru, zkontroluj zapoojeni.");
+    while(1);
+  }
+
+  // Wait for the sensor to be ready
+  while(!ccs.available());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(ccs.available()){
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+  delay(500);
 }
